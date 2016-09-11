d82801b09e357460465f28ff76b8b5159cc831d7
Harlan James <root@aboyandhisgnu.org>
2016-09-10 23:33:06 -0400

    Added logging script, doesn't work on Debian?

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-'use strict';
+'use strict'
 
-const Sleepytime = require('./lib/sleepytime');
+const Sleepytime = require('./lib/sleepytime')
 
-const SERVER_ADDR = '127.0.0.1';
-const SERVER_PORT = 1990;
+const SERVER_ADDR = '127.0.0.1'
+const SERVER_PORT = 1990
 
-const worker_one = new Sleepytime(SERVER_ADDR, SERVER_PORT, 0).route().run();
-const worker_two = new Sleepytime(SERVER_ADDR, SERVER_PORT, 1).route().run();
+const worker_one = new Sleepytime(SERVER_ADDR, SERVER_PORT, 0).route().run()
+const worker_two = new Sleepytime(SERVER_ADDR, SERVER_PORT, 1).route().run()
+
+process.title = "sleepytime"
